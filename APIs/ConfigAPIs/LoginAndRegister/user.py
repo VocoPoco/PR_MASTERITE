c5b7f22ad04621a712dc6601fc9a15eb0fac7c4f
VocoPoco <vik.penev@gmail.com>
2023-03-10 01:28:04 +0200

    Installed Django, Created Env

--- APIs/ConfigAPIs/LoginAndRegister/user.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    first = models.CharField(max_length=255)
-    last = models.CharField(max_length=255)
-    username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    type = models.BooleanField(default=False)
