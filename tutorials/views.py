f321d7c570437c205428a10689638c4a8b8a5cd1
VocoPoco <vik.penev@gmail.com>
2023-03-10 03:58:33 +0200

    directory arrangements

--- tutorials/views.py
+++ tutorials/views.py
@@ -4,5 +4,29 @@ from django.template import loader
 
 
 def home(request):
-  template = loader.get_template('templates/home.html')
+  template = loader.get_template('templates/mainLoginPage.html')
+  return HttpResponse(template.render())
+
+def about(request):
+  template = loader.get_template('templates/aboutPage.html')
+  return HttpResponse(template.render())
+
+def login(request):
+  template = loader.get_template('templates/loginPage.html')
+  return HttpResponse(template.render())
+
+def register(request):
+  template = loader.get_template('templates/registerPage.html')
+  return HttpResponse(template.render())
+
+def tutorial_1(request):
+  template = loader.get_template('templates/tutorial1.html')
+  return HttpResponse(template.render())
+
+def tutorial_2(request):
+  template = loader.get_template('templates/tutorial2.html')
+  return HttpResponse(template.render())
+
+def tutorial_3(request):
+  template = loader.get_template('templates/tutorial3.html')
   return HttpResponse(template.render())
