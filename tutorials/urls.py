5ca99275e1b3839e6dfb26efc9724c4d3760ed37
VocoPoco <vik.penev@gmail.com>
2023-03-10 05:03:52 +0200

    Edited Images Folder and url.py

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('/home', views.home, name='home'),
     path('', views.about , name='about'),
     path('', views.login , name='login'),
     path('', views.register, name='register'),
-    path('', views.tutorial1, name='tutorial1'),
-    path('', views.tutorial2 , name='tutorial2'),
-    path('', views.tutorial3 , name='tutorial3'),
+    path('', views.tutorial_1, name='tutorial_1'),
+    path('', views.tutorial_2 , name='tutorial_2'),
+    path('', views.tutorial_3 , name='tutorial_3'),
 ]
--- tutorials/views.py
+++ tutorials/views.py
@@ -4,29 +4,29 @@ from django.template import loader
 
 
 def home(request):
-  template = loader.get_template('templates/mainLoginPage.html')
+  template = loader.get_template('mainLoginPage.html')
   return HttpResponse(template.render())
 
 def about(request):
-  template = loader.get_template('templates/aboutPage.html')
+  template = loader.get_template('aboutPage.html')
   return HttpResponse(template.render())
 
 def login(request):
-  template = loader.get_template('templates/loginPage.html')
+  template = loader.get_template('loginPage.html')
   return HttpResponse(template.render())
 
 def register(request):
-  template = loader.get_template('templates/registerPage.html')
+  template = loader.get_template('registerPage.html')
   return HttpResponse(template.render())
 
 def tutorial_1(request):
-  template = loader.get_template('templates/tutorial1.html')
+  template = loader.get_template('tutorial1.html')
   return HttpResponse(template.render())
 
 def tutorial_2(request):
-  template = loader.get_template('templates/tutorial2.html')
+  template = loader.get_template('tutorial2.html')
   return HttpResponse(template.render())
 
 def tutorial_3(request):
-  template = loader.get_template('templates/tutorial3.html')
+  template = loader.get_template('tutorial3.html')
   return HttpResponse(template.render())
